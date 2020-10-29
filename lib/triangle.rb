cbef2219c519dc55b9f76220af7e02a0e58b00b2
jomapormentilla <jomapormentilla@gmail.com>
2020-10-29 14:37:42 -0400

    commit tree 7a34180b7b58d8d1531445f01be0275dab31af85

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene, :one, :two, :three
+  
+  def initialize( one, two, three )
+    @one = one
+    @two = two
+    @three = three
+  end
+
+  def kind
+    sides = [@one, @two, @three]
+        # no size                         # negative numbers                # inequality rule
+    if sides.all?{ |side| side == 0 } || sides.any?{ |side| side < 0 } || sides.sort[0] + sides.sort[1] <= sides.sort[2]
+      raise TriangleError
+    elsif sides.all?{ |side| side == @one }
+      :equilateral
+    elsif (sides[0] != sides[1] && sides[1] == sides[2] || sides[0] == sides[1] && sides[1] != sides[2] || sides[0] == sides[2] && sides[1] != sides[2])
+      :isosceles
+    elsif sides[0] != sides[1] && sides[1] != sides[2]
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
